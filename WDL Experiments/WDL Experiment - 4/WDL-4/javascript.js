387a13813e4968c42c45276861db9c64d0c00875
Amey-Thakur <ameythakur20@gmail.com>
2025-12-21 22:24:41 -0500

    WDL

--- WDL Experiments/WDL Experiment - 4/WDL-4/javascript.js
+++ WDL Experiments/WDL Experiment - 4/WDL-4/javascript.js
@@ -1,23 +1,55 @@
-function myFunction1(){
+/**
+ * Author: Amey Thakur
+ * GitHub: https://github.com/Amey-Thakur
+ * Repository: https://github.com/Amey-Thakur/WEB-DESIGNING-LAB
+ * Description: JavaScript Logic for Interest Calculator (Experiment 4)
+ * 
+ * This script contains functions to calculate Simple Interest (SI) 
+ * and Compound Interest (CI) based on user input.
+ */
+
+/**
+ * Calculates Simple Interest (SI)
+ * Formula: SI = (P * R * T) / 100
+ */
+function myFunction1() {
+  // Get values from input fields
   var p = document.getElementById('sprincipal').value;
   var r = document.getElementById('srate').value;
   var t = document.getElementById('syear').value;
-  var inr = p*r*t;
-  var inrst = inr/100;
-  document.getElementById('SI').innerHTML=  "<span class='color-class'>Simple Interest is "+inrst+"</span>";
+
+  // Mathematical logical steps
+  var inr = p * r * t;
+  var inrst = inr / 100;
+
+  // Display the result in the corresponding paragraph tag
+  document.getElementById('SI').innerHTML = "<span class='color-class'>Simple Interest is ₹" + inrst + "</span>";
 }
 
-function myFunction2(){
+/**
+ * Calculates Compound Interest (CI)
+ * Formula: A = P(1 + r/n)^(nt)
+ */
+function myFunction2() {
+  // Get values from input fields
   var p = document.getElementById('cprincipal').value;
   var r = document.getElementById('crate').value;
   var t = document.getElementById('cyear').value;
   var n = document.getElementById('cnd').value;
-  var r1 = r/100;
-  var nbr = r1/n;
-  var x = (1+nbr);
-  var nt = n*t;
-  var xnt = Math.pow(x,nt);
-  var cmpndint = p*xnt;
+
+  // Calculation logic
+  var r1 = r / 100;    // Convert rate to decimal
+  var nbr = r1 / n;    // Rate divided by compounding per year
+  var x = (1 + nbr);   // Base part of the power function
+  var nt = n * t;      // Total number of compounding periods
+
+  // Calculate total amount using Math.pow
+  var xnt = Math.pow(x, nt);
+  var cmpndint = p * xnt;
+
+  // Format to 2 decimal places for currency accuracy
   var z = cmpndint.toFixed(2);
-  document.getElementById('CI').innerHTML=  "<span class='color-class'>Compound Interest is "+z+"</span>";
+
+  // Display the result
+  document.getElementById('CI').innerHTML = "<span class='color-class'>Compound Interest is ₹" + z + "</span>";
 }
