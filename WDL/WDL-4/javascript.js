0dbb0ca936b040dd4e7ea59eebfbefd6440c8d58
Amey-Thakur <ameythakur20@gmail.com>
2025-12-22 00:08:20 -0500

    WDL

--- WDL/WDL-4/javascript.js
+++ WDL/WDL-4/javascript.js
@@ -1,23 +1,43 @@
-function myFunction1(){
+/*
+    Author: Amey Thakur
+    GitHub: https://github.com/Amey-Thakur
+    Repository: https://github.com/Amey-Thakur/WEB-DESIGNING-LAB
+    Description: External JavaScript for Interest Calculator (javascript.js)
+    Task: Logic for Simple and Compound Interest calculation.
+*/
+
+/* Function to Calculate Simple Interest */
+function myFunction1() {
   var p = document.getElementById('sprincipal').value;
   var r = document.getElementById('srate').value;
   var t = document.getElementById('syear').value;
-  var inr = p*r*t;
-  var inrst = inr/100;
-  document.getElementById('SI').innerHTML=  "<span class='color-class'>Simple Interest is "+inrst+"</span>";
+
+  // Basic Interest Logic: (P * R * T) / 100
+  var inr = p * r * t;
+  var inrst = inr / 100;
+
+  // Output Result
+  document.getElementById('SI').innerHTML = "<span class='color-class'>Simple Interest is " + inrst + "</span>";
 }
 
-function myFunction2(){
+/* Function to Calculate Compound Interest */
+function myFunction2() {
   var p = document.getElementById('cprincipal').value;
   var r = document.getElementById('crate').value;
   var t = document.getElementById('cyear').value;
   var n = document.getElementById('cnd').value;
-  var r1 = r/100;
-  var nbr = r1/n;
-  var x = (1+nbr);
-  var nt = n*t;
-  var xnt = Math.pow(x,nt);
-  var cmpndint = p*xnt;
+
+  // Rate per decimal
+  var r1 = r / 100;
+  var nbr = r1 / n;
+  var x = (1 + nbr);
+  var nt = n * t;
+  var xnt = Math.pow(x, nt);
+
+  // Total Amount Calculation
+  var cmpndint = p * xnt;
   var z = cmpndint.toFixed(2);
-  document.getElementById('CI').innerHTML=  "<span class='color-class'>Compound Interest is "+z+"</span>";
+
+  // Output Result
+  document.getElementById('CI').innerHTML = "<span class='color-class'>Compound Interest is " + z + "</span>";
 }
